9130c7018223d2a4913c957c79781f1c8608c4ec
Oscar Coniel <oscarconiel1@Oscars-MacBook-Pro.local>
2020-07-30 14:21:56 +0300

    fix: add @slash/material-ui package

--- packages/plugin-block/src/BlockPlugin.story.tsx
+++ packages/plugin-block/src/BlockPlugin.story.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Node } from 'slate';
 import { Editor } from '@slash/editor';
+import * as components from '@slash/material-ui';
 import BlockPlugin from './BlockPlugin';
 
 export default { title: 'Plugins|Block' };
 
 const Block = BlockPlugin();
-const CustomBlock = BlockPlugin({});
 
 export const WithBlockPlugin: React.FC = () => {
   const [value, setValue] = useState<Node[]>([
@@ -18,27 +18,9 @@ export const WithBlockPlugin: React.FC = () => {
 
   return (
     <Editor
+      components={components}
       value={value}
       plugins={[Block]}
-      onChange={(newValue): void => {
-        setValue(newValue);
-      }}
-    />
-  );
-};
-
-export const WithCustomisedBlockPlugin: React.FC = () => {
-  const [value, setValue] = useState<Node[]>([
-    {
-      type: 'text',
-      children: [{ text: 'I use the BlockPlugin with custom options.' }],
-    },
-  ]);
-
-  return (
-    <Editor
-      value={value}
-      plugins={[CustomBlock]}
       onChange={(newValue): void => {
         setValue(newValue);
       }}
