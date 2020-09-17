797ee0e7f2eca16539186bb8ad1222f47fbe63d1
Oscar Coniel <oscarconiel1@Oscars-MacBook-Pro.local>
2020-09-18 01:29:39 +0300

    fix(plugin-equation): export components

--- packages/plugin-equation/src/index.ts
+++ packages/plugin-equation/src/index.ts
@@ -1,2 +1,8 @@
 export { default } from './EquationPlugin';
 export * from './EquationPlugin';
+
+export { default as ElementEquation } from './ElementEquationBlock';
+export * from './ElementEquationBlock';
+
+export { default as ElementEquationInline } from './ElementEquationInline';
+export * from './ElementEquationInline';
