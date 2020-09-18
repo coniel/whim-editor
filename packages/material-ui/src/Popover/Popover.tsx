e5449a69ba529e1dae0ec31e5dbf2fa1fd8b75ee
Oscar Coniel <oscarconiel1@Oscars-MacBook-Pro.local>
2020-09-18 12:41:34 +0300

    style(materila-ui): remove Popover paper min width

--- packages/material-ui/src/Popover/Popover.tsx
+++ packages/material-ui/src/Popover/Popover.tsx
@@ -20,7 +20,6 @@ const useStyles = makeStyles(() =>
   createStyles({
     root: {},
     paper: {
-      minWidth: 200,
       borderRadius: 3,
       boxShadow:
         'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
