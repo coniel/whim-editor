73a29a62ddfabf0ce8a9e2ed6c7946c7712c00e7
Oscar Coniel <oscarconiel1@Oscars-MacBook-Pro.local>
2020-10-07 23:25:37 +0300

    fix(plugin-rich-text): check if deserialized element has style property before reading it

--- packages/plugin-rich-text/src/RichTextPlugin.tsx
+++ packages/plugin-rich-text/src/RichTextPlugin.tsx
@@ -159,6 +159,10 @@ const createRichTextPlugin = (
       })),
       markDeserializers: {
         '*': (el): DeserializeMarkValue => {
+          if (!el.style) {
+            return;
+          }
+
           if (
             ['600', '700', '800', '900', 'bold'].includes(el.style.fontWeight)
           ) {
