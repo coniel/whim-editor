61864484ca9715a5bfd13938795f932d0ae6daf5
Oscar Coniel <oscar@coniel.com>
2021-06-25 00:03:41 +0300

    fix(plugin-to-do-list): allow toggling checkbox using keyboard

--- packages/plugin-to-do-list/src/ToDoListPlugin.tsx
+++ packages/plugin-to-do-list/src/ToDoListPlugin.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import isHotkey from 'is-hotkey';
 import {
   BraindropEditorPluginFactory,
   BraindropEditor,
+  getBlockAbove,
+  isNodeType,
 } from '@braindrop-editor/core';
 import { ReactEditor } from 'slate-react';
 import { Transforms } from 'slate';
@@ -9,6 +12,7 @@ import { ElementToDoList } from './ElementToDoList';
 import { ToDoListElement } from './ToDoListPlugin.types';
 
 export interface ToDoListPluginOptions {
+  type?: string;
   checkedColor?: string;
   uncheckedColor?: string;
   onToggleChecked?: (element: ToDoListElement, checked: boolean) => void;
@@ -19,6 +23,7 @@ export const createToDoListPlugin = (
 ): BraindropEditorPluginFactory<ToDoListElement> => (
   editor: BraindropEditor,
 ) => {
+  const type = options.type || 'to-do';
   function toggleDone(element: ToDoListElement) {
     const path = ReactEditor.findPath(editor, element);
 
@@ -38,9 +43,17 @@ export const createToDoListPlugin = (
   }
 
   return {
+    onKeyDown: (event) => {
+      if (isHotkey('mod+Enter', (event as unknown) as KeyboardEvent)) {
+        const parent = getBlockAbove(editor);
+        if (isNodeType(parent, { allow: [type] })) {
+          toggleDone(parent[0] as ToDoListElement);
+        }
+      }
+    },
     elements: [
       {
-        type: 'to-do',
+        type,
         returnBehaviour: 'same-type',
         component: (props) => (
           <ElementToDoList
